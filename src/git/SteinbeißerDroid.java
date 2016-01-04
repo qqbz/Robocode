2bbec527ab809a309ff8b26d7835bc1dd434d2d0
Alex <>
2016-01-04 21:21:12 +0100

    v0.31

--- src/git/SteinbeißerDroid.java
+++ src/git/SteinbeißerDroid.java
@@ -9,7 +9,7 @@ import robocode.MessageEvent;
 import robocode.TeamRobot;
 import robocode.DeathEvent;
 import static robocode.util.Utils.normalRelativeAngleDegrees;
-
+//kommunikation mit 
 public class SteinbeißerDroid extends TeamRobot implements Droid {
 
     public Hashtable<String, RobotInfo> robotsList = new Hashtable<String, RobotInfo>();
@@ -91,20 +91,20 @@ public class SteinbeißerDroid extends TeamRobot implements Droid {
 
     }
 
-    public double normalRelativeAngle(double angle) {
-        if (angle > -180 && angle <= 180) {
-            return angle;
-        }
-        double fixedAngle = angle;
-
-        while (fixedAngle <= -180) {
-            fixedAngle += 360;
-        }
-        while (fixedAngle > 180) {
-            fixedAngle -= 360;
-        }
-        return fixedAngle;
-    }
+//    public double normalRelativeAngle(double angle) {
+//        if (angle > -180 && angle <= 180) {
+//            return angle;
+//        }
+//        double fixedAngle = angle;
+//
+//        while (fixedAngle <= -180) {
+//            fixedAngle += 360;
+//        }
+//        while (fixedAngle > 180) {
+//            fixedAngle -= 360;
+//        }
+//        return fixedAngle;
+//    }
     
  //http://www.jasonsjava.com/?cat=2
     private void goTo(Point2D destination) {
@@ -140,16 +140,12 @@ public class SteinbeißerDroid extends TeamRobot implements Droid {
     @Override
     public void onHitRobot(HitRobotEvent e) {
         if (isTeammate(e.getName())) {
-//            reverseDirection();
-            turnLeft(e.getBearing() + 90);
-            ahead(100);
-            System.out.println("I have hit the Leader");
+            reverseDirection();
+//            turnLeft(e.getBearing() + 90);
+//            ahead(100);
+//            System.out.println("I have hit the Leader");
         }
-        if (!isTeammate(e.getName()) && e.isMyFault()) {
-            setBack(5);
-            execute();
-            fire(3);
-        }
+
 
     }
 
